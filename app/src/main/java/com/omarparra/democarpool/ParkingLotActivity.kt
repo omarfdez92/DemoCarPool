08ac19c670f0d35f8c90adcd96581acd8fd36861
Omar Parra <omarfdez92@gmail.com>
2019-11-29 02:31:15 -0600

    New activity for parking lot

---
+++ app/src/main/java/com/omarparra/democarpool/ParkingLotActivity.kt
@@ -0,0 +1,12 @@
+package com.omarparra.democarpool
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ParkingLotActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_parking_lot)
+    }
+}
